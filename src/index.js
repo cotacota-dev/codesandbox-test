afe741f08b37ed89875c0091f8c34ba25ab21851
cotacota-dev <98221551+cotacota-dev@users.noreply.github.com>
2022-01-30 22:39:07 +0900

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,65 @@
 import "./styles.css";
 
-console.log("test");
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  alert(inputText);
+  document.getElementById("add-text").value = "";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  console.log(div);
+
+  //liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+  console.log(li);
+
+  // 未完了のリストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+
+  //完了　ボタン
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    // div作成
+    const div = document.createElement("div");
+    div.className = "list-row";
+
+    // li作成
+    const li = document.createElement("li");
+    li.innerText = inputText;
+
+    // button作成
+    const returnButton = document.createElement("button");
+    returnButton.innerText = "戻る";
+    returnButton.addEventListener("click", () => {
+      alert();
+    });
+
+    document.getElementById("complete-list").appendChild(div);
+    div.appendChild(li);
+    div.appendChild(returnButton);
+
+    const completeTarget = completeButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(completeTarget);
+  });
+
+  //削除　ボタン
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
